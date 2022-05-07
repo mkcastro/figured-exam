996117de34312e9ad009339cd91b27e06c61205b
Mike Castro <mkcastro24@gmail.com>
2022-05-08 01:02:12 +0800

    init inventory movement model

---
+++ database/migrations/2022_05_06_112548_create_inventory_movements_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory_movements', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->unsignedInteger('quantity');
+            $table->unsignedInteger('price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory_movements');
+    }
+};
