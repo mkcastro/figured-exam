2ea0b34c2aa27338240327ff651a5862160baa9f
Mike Castro <mkcastro24@gmail.com>
2022-05-08 08:15:17 +0800

    add browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,6 +18,10 @@ mix.js('resources/js/app.js', 'public/js').vue()
     ])
     .webpackConfig(require('./webpack.config'));
 
+if (!mix.inProduction()) {
+    mix.browserSync('localhost');
+}
+
 if (mix.inProduction()) {
     mix.version();
 }
