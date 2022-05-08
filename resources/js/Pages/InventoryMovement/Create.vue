b146996c2eb7d8c35f3a43503c45122f2fb9978f
Mike Castro <mkcastro24@gmail.com>
2022-05-09 06:11:51 +0800

    improve upload inventory movements ux

--- resources/js/Pages/InventoryMovement/Create.vue
+++ resources/js/Pages/InventoryMovement/Create.vue
@@ -1,5 +1,5 @@
 <template>
-  <app-layout title="Profile">
+  <app-layout title="Upload Inventory Profile">
     <template #header>
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
         Upload Inventory Movements
--- resources/js/Pages/InventoryMovement/Partials/UploadInventoryMovementForm.vue
+++ resources/js/Pages/InventoryMovement/Partials/UploadInventoryMovementForm.vue
@@ -1,21 +1,61 @@
 <template>
   <form @submit.prevent="submit">
-    <div class="flex flex-col">
-      <input
-        type="file"
-        @input="form.inventory_movements_file = $event.target.files[0]"
-      />
+    <div class="flex flex-col justify-center">
       <div class="flex">
-        <progress
-          v-if="form.progress"
-          :value="form.progress.percentage"
-          max="100"
-        >
-          {{ form.progress.percentage }}%
-        </progress>
-        <button type="submit">Submit</button>
+        <div class="mb-3 w-96">
+          <label
+            for="formFile"
+            class="form-label inline-block mb-2 text-gray-700"
+            >Upload Inventory Movement File</label
+          >
+          <div class="flex">
+            <input
+              class="
+                form-control
+                block
+                w-full
+                px-3
+                py-1.5
+                text-base
+                font-normal
+                text-gray-700
+                bg-white bg-clip-padding
+                border border-solid border-gray-300
+                rounded
+                transition
+                ease-in-out
+                m-0
+                focus:text-gray-700
+                focus:bg-white
+                focus:border-blue-600
+                focus:outline-none
+              "
+              type="file"
+              @input="form.inventory_movements_file = $event.target.files[0]"
+            />
+            <!-- TODO: add progress bar -->
+            <button
+              type="submit"
+              class="
+                ml-2
+                bg-blue-500
+                hover:bg-blue-700
+                text-white
+                font-bold
+                py-2
+                px-4
+                rounded
+              "
+            >
+              Upload
+            </button>
+          </div>
+        </div>
       </div>
     </div>
+    <ul>
+      <li v-for="(error, key) in form.errors" :key="key">{{ error }}</li>
+    </ul>
   </form>
 </template>
 
